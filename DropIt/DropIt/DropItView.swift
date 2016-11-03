9e713405f4dd85037fc48a44d4620fd57818fcd2
wei.mao <wei.mao@jimubox.com>
2016-11-03 10:13:40 +0800

    Lession 14 Animation and Core Motion

---
+++ DropIt/DropIt/DropItView.swift
@@ -0,0 +1,21 @@
+//
+//  DroItView.swift
+//  DropIt
+//
+//  Created by Mao on 03/11/2016.
+//  Copyright © 2016 cdts. All rights reserved.
+//
+
+import UIKit
+
+class DropItView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DropIt/DropIt/DropItViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DropItViewController.swift
+//  DropIt
+//
+//  Created by Mao on 03/11/2016.
+//  Copyright © 2016 cdts. All rights reserved.
+//
+
+import UIKit
+
+class DropItViewController: UIViewController {
+
+    @IBOutlet weak var gameView: DropItView!
+    
+}
